1ad24d925d4fe6e51d0543efdaf33319b6b06877
Benedict <bobuzytech@gmail.com>
2024-05-16 10:36:34 +0000

    Accept and use kwargs argument to create new BaseModel instance

--- models/base_model.py
+++ models/base_model.py
@@ -11,12 +11,17 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initializes a new instance of the BaseModel class."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
         if kwargs:
-            pass
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            dt_format = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, dt_format)
+                else:
+                    self.__dict__[key] = value
 
     def __str__(self):
         """Returns a string representation of the BaseModel class."""
