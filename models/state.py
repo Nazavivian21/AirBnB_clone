e4099cba5b96883b1b65096ac436bd9891ca371c
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-05-19 00:48:58 +0100

    Fix a bug in state.py

--- models/state.py
+++ models/state.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-"""Defines the State class."""
 from base_model import BaseModel
 
+
 class State(BaseModel):
-    """Represents a state."""
-    name = ""
+    """The State class inherits from BaseModel and defines \
+          attributes for a state."""
+
+    def __init__(self, name=""):
+        """Initializes a new instance of the State class."""
+        super().__init__()
+        self.name = name
+
+    def __str__(self):
+        """Returns a string representation of the State class."""
+        return "[State] ({}) - Name: {}".format(self.id, self.name)
