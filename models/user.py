a7bd559979cce439ee2c30e6a7dbb76d8fdb83a3
Benedict <bobuzytech@gmail.com>
2024-05-19 19:44:01 +0000

    Update user class

--- models/user.py
+++ models/user.py
@@ -1,32 +1,14 @@
 #!/usr/bin/python3
 
-from datetime import datetime
-from uuid import uuid4
+
+"""User class module"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """The User class inherits from BaseModel and defines attributes for a user."""
+    """The User class definition"""
 
-    def __init__(self, email="", password="", first_name="", last_name=""):
-        """Initializes a new instance of the User class."""
-        super().__init__()
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
-
-    def __str__(self):
-        """Returns a string representation of the User class."""
-        return "[User] ({}) - Email: {}, First Name: {}, Last Name: {}".format(
-            self.id, self.email, self.first_name, self.last_name
-        )
-
-    def set_password(self, password):
-        """Sets the password for the user."""
-        self.password = password
-
-    def set_name(self, first_name, last_name):
-        """Sets the first name and last name for the user."""
-        self.first_name = first_name
-        self.last_name = last_name
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
