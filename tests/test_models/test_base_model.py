9c9f638140e0d0a9a170a96dfffbf1f3ed0037d3
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-05-19 17:28:54 +0100

    Add pycodestyle validation to test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,6 +3,7 @@ import unittest
 from datetime import datetime, timedelta
 from models.base_model import BaseModel
 
+
 class TestBaseModel(unittest.TestCase):
 
     def test_to_dict(self):
@@ -11,8 +12,12 @@ class TestBaseModel(unittest.TestCase):
         instance_dict = instance.to_dict()
         self.assertEqual(instance_dict["__class__"], "BaseModel")
         self.assertEqual(instance_dict["id"], instance.id)
-        self.assertEqual(instance_dict["created_at"], instance.created_at.isoformat())
-        self.assertEqual(instance_dict["updated_at"], instance.updated_at.isoformat())
+        self.assertEqual(
+            instance_dict["created_at"], instance.created_at.isoformat()
+        )
+        self.assertEqual(
+            instance_dict["updated_at"], instance.updated_at.isoformat()
+        )
 
     def test_from_dict(self):
         """Test recreating a BaseModel instance from a dictionary"""
@@ -33,10 +38,14 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(recreated_instance.custom_attribute, "value")
 
     def test_datetime_attributes(self):
-        """Test converting and recreating an instance with datetime attributes"""
+        """Test converting and recreating an instance \
+              with datetime attributes"""
         created_at = datetime.now()
         updated_at = created_at + timedelta(days=1)
-        instance = BaseModel(created_at=created_at.isoformat(), updated_at=updated_at.isoformat())
+        instance = BaseModel(
+            created_at=created_at.isoformat(),
+            updated_at=updated_at.isoformat(),
+        )
         instance_dict = instance.to_dict()
         recreated_instance = BaseModel(**instance_dict)
         self.assertEqual(recreated_instance.created_at, created_at)
@@ -44,8 +53,10 @@ class TestBaseModel(unittest.TestCase):
 
     def test_custom_subclass(self):
         """Test converting and recreating an instance of a custom subclass"""
+
         class CustomModel(BaseModel):
             """A custom subclass of BaseModel"""
+
             pass
 
         instance = CustomModel()
