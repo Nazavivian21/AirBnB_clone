685cde836b0cd818b0d0c787556a9f51e14cf72e
Benedict <bobuzytech@gmail.com>
2024-05-19 19:44:30 +0000

    Update city class

--- models/city.py
+++ models/city.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
+"""the City class module"""
 
-from base_model import BaseModel
+
+from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """The City class inherits from BaseModel and defines \
-          attributes for a city."""
+    """City class definition"""
 
-    def __init__(self, state_id="", name=""):
-        """Initializes a new instance of the City class."""
-        super().__init__()
-        self.state_id = state_id
-        self.name = name
-
-    def __str__(self):
-        """Returns a string representation of the City class."""
-        return "[City] ({}) - Name: {}, State ID: {}".format(
-            self.id, self.name, self.state_id
-        )
+    state_id = ""
+    name = ""
