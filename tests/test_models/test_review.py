db83e4d4a3f40b7964cfc711742fa83451330cb9
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-05-20 02:23:35 +0100

    Add test for review.py

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import unittest
+from models.review import Review
+
+
+class TestReview(unittest.TestCase):
+    def test_attributes(self):
+        """Test Review class attributes"""
+        review = Review()
+        self.assertEqual(review.place_id, "")
+        self.assertEqual(review.user_id, "")
+        self.assertEqual(review.text, "")
