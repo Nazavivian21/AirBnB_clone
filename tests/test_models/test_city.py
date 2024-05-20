836bdde3001af8eb958820a02c84a0a406a21c84
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-05-20 02:22:43 +0100

    Add test for city.py

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import unittest
+from models.city import City
+
+
+class TestCity(unittest.TestCase):
+    def test_attributes(self):
+        """Test City class attributes"""
+        city = City()
+        self.assertEqual(city.state_id, "")
+        self.assertEqual(city.name, "")
